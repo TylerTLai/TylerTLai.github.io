986abffc0919c12a5cfb14214481afb8463e305b
Tyler Lai <lai.t.tyler@gmail.com>
2020-09-07 15:20:33 -0500

    Updates

---
+++ precache-manifest.93445ca90bbec8af62b787d6d0540146.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09a2017c206450ee9bbefe43cf27012",
+    "url": "/film-findr-2/index.html"
+  },
+  {
+    "revision": "2b32029f118ffa4ea86e",
+    "url": "/film-findr-2/static/css/2.d4311649.chunk.css"
+  },
+  {
+    "revision": "2b32029f118ffa4ea86e",
+    "url": "/film-findr-2/static/js/2.dad88d70.chunk.js"
+  },
+  {
+    "revision": "f08e720869e7a847549fb58c8267edec",
+    "url": "/film-findr-2/static/js/2.dad88d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621f99110ce9b961d958",
+    "url": "/film-findr-2/static/js/main.ae80aab2.chunk.js"
+  },
+  {
+    "revision": "a2bf8a8d08991937cd02",
+    "url": "/film-findr-2/static/js/runtime-main.861d4d53.js"
+  },
+  {
+    "revision": "4b3ce967729819d98f9e7362bd29df2f",
+    "url": "/film-findr-2/static/media/logo.4b3ce967.svg"
+  },
+  {
+    "revision": "feaa40cced5a880a4a403a5d30bc28e2",
+    "url": "/film-findr-2/static/media/poster.feaa40cc.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/film-findr-2/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/film-findr-2/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/film-findr-2/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/film-findr-2/static/media/slick.f97e3bbf.svg"
+  }
+]);
