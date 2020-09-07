d025661db6872566e89eaa2d2b56881df824422c
Tyler Lai <lai.t.tyler@gmail.com>
2020-09-07 16:30:34 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import rootReducer from './store/reducers';
 import 'normalize.css';
 
